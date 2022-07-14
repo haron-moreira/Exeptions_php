a5342429f3a40239cdd4e68361dfc7e3fadbcf66
Haron Moreira <haron.moreira@surf.com.br>
2022-07-14 12:26:36 -0300

    Initial commit

--- pilha.php
+++ pilha.php
@@ -3,16 +3,25 @@
 function funcao1()
 {
     echo 'Entrei na função 1' . PHP_EOL;
-    funcao2();
+
+    try {
+        funcao2();
+    } catch (Throwable $problema) {
+        echo $problema->getMessage() . PHP_EOL;
+        echo $problema->getLine() . PHP_EOL;
+        echo $problema->getTraceAsString() . PHP_EOL;
+    }
+
     echo 'Saindo da função 1' . PHP_EOL;
 }
 
 function funcao2()
 {
     echo 'Entrei na função 2' . PHP_EOL;
-    for ($i = 1; $i <= 5; $i++) {
-        echo $i . PHP_EOL;
-    }
+
+    intdiv(1, 0);
+    throw new BadFunctionCallException('Essa é a mensagem de exceção');
+
     echo 'Saindo da função 2' . PHP_EOL;
 }
 
